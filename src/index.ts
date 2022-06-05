6765d974385e62af3473ed8cbd8fe4584ec08146
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-06-05 01:47:56 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,126 @@
+// Clase 13 - Lunes 30 de mayo de 2022
+//Ejercicios de Repaso (NO OBLIGATORIOS)
+//Ejercicio 2:
+/*Una materia de la carrera de Ingeniería de Sistemas 
+tiene 30 alumnos, divididos en 3 comisiones. 
+Estos arreglos contienen las notas finales de los alumnos 
+en cada comisión :
+comisionA = [8, 5, 7, 10, 9, 2, 4, 8, 6, 8]
+comisionB = [3, 6, 8, 5, 4, 7, 7, 4, 6, 8]
+comisionC = [7, 6, 9, 8, 9, 8, 5, 7, 10, 9]
+
+1. Crear una función que devuelva el mayor promedio 
+y a qué comisión corresponde.
+2. Crear una función que devuelva el menor promedio 
+y a qué comisión corresponde.
+3. Teniendo en cuenta que la materia se aprueba con 6, 
+crear una función que devuelva la cantidad de desaprobados 
+en una comisión, mostrar el resultado para cada comisión
+4. Crear una función que devuelva la menor nota de una 
+comisión, mostrar el resultado para cada comisión.*/
+
+let comisionA: number[] = [8, 5, 7, 10, 9, 2, 4, 8, 6, 8];
+let comisionB: number[] = [3, 6, 8, 5, 4, 7, 7, 4, 6, 8];
+let comisionC: number[] = [7, 6, 9, 8, 9, 8, 5, 7, 10, 9];
+let indice: number;
+
+/*1. Crear una función que devuelva el mayor promedio 
+y a qué comisión corresponde.*/
+function promedioNota(notas: number[]): number {
+  let suma: number = 0;
+  let promedio: number = notas.length;
+  for (indice = 0; indice < notas.length; indice++) suma += notas[indice];
+  return suma / promedio;
+}
+
+function mayorPromedio(
+  comisionA: number[],
+  comisionB: number[],
+  comisionC: number[]
+): string {
+  let promedioA: number = promedioNota(comisionA);
+  let promedioB: number = promedioNota(comisionB);
+  let promedioC: number = promedioNota(comisionC);
+  let texto: string = "";
+  if (promedioA > promedioB && promedioA > promedioC) {
+    texto =
+      "El mayor promedio es: " + promedioA + " y corresponde a la comisión A";
+  } else if (promedioB > promedioA && promedioB > promedioC) {
+    texto =
+      "El mayor promedio es: " + promedioB + " y corresponde a la comisión B";
+  } else {
+    texto =
+      "El mayor promedio es: " + promedioC + " y corresponde a la comisión C";
+  }
+  return texto;
+}
+
+/*2. Crear una función que devuelva el menor promedio 
+y a qué comisión corresponde.*/
+function menorPromedio(
+  comisionA: number[],
+  comisionB: number[],
+  comisionC: number[]
+): string {
+  let promedioA: number = promedioNota(comisionA);
+  let promedioB: number = promedioNota(comisionB);
+  let promedioC: number = promedioNota(comisionC);
+  let texto: string = "";
+  if (promedioA < promedioB && promedioA < promedioC) {
+    texto =
+      "El menor promedio es: " + promedioA + " y corresponde a la comisión A";
+  } else if (promedioB < promedioA && promedioB < promedioC) {
+    texto =
+      "El menor promedio es: " + promedioB + " y corresponde a la comisión B";
+  } else {
+    texto =
+      "El menor promedio es: " + promedioC + " y corresponde a la comisión C";
+  }
+  return texto;
+}
+
+/*3. Teniendo en cuenta que la materia se aprueba con 6, 
+crear una función que devuelva la cantidad de desaprobados 
+en una comisión, mostrar el resultado para cada comisión.*/
+function totalDesaprobados(notas: number[]): number {
+  let cantidadDesaprobados: number = 0;
+  for (indice = 0; indice < notas.length; indice++) {
+    if (notas[indice] < 6) {
+      cantidadDesaprobados++;
+    }
+  }
+  return cantidadDesaprobados;
+}
+
+/*4. Crear una función que devuelva la menor nota de una 
+comisión, mostrar el resultado para cada comisión.*/
+function menorNota(notas: number[]): number {
+  let menor: number = 10;
+  for (indice = 0; indice < notas.length; indice++) {
+    if (notas[indice] < menor) {
+      menor = notas[indice];
+    }
+  }
+  return menor;
+}
+
+console.log(mayorPromedio(comisionA, comisionB, comisionC));
+console.log(menorPromedio(comisionA, comisionB, comisionC));
+console.log(
+  "La comisión A tiene " + totalDesaprobados(comisionA) + " desaprobados"
+);
+console.log(
+  "La comisión B tiene " + totalDesaprobados(comisionB) + " desaprobados"
+);
+console.log(
+  "La comisión C tiene " + totalDesaprobados(comisionC) + " desaprobados"
+);
+console.log("La menor nota de la comisón A es: " + menorNota(comisionA));
+console.log("La menor nota de la comisón A es: " + menorNota(comisionB));
+console.log("La menor nota de la comisón A es: " + menorNota(comisionC));
+
+/*
+Ejercicio2 resuelto por el alumno Mariano Carbajo
+Fuente: CodeSandbox | GitHub
+MarianoCarbajo/EjerciciosPracticaProg7/main 
+*/
